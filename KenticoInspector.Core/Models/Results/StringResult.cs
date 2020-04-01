d6de4cf1626988e2268634faee644c18acde66e4
Yuriy Sountsov <yuriys@kentico.com>
2020-04-01 15:10:48 -0400

    Ensure that most result properties are HTML encoded

--- KenticoInspector.Core/Models/Results/StringResult.cs
+++ KenticoInspector.Core/Models/Results/StringResult.cs
@@ -1,11 +1,13 @@
-﻿namespace KenticoInspector.Core.Models.Results
+﻿using System.Web;
+
+namespace KenticoInspector.Core.Models.Results
 {
     public class StringResult : Result
     {
-        public string String { get; set; }
+        public string String { get; }
 
         public override bool HasData => !string.IsNullOrWhiteSpace(String);
 
-        internal StringResult(string stringData) => String = stringData;
+        internal StringResult(string stringData) => String = HttpUtility.HtmlEncode(stringData);
     }
 }
--- KenticoInspector.Core/Models/Results/TableResult.cs
+++ KenticoInspector.Core/Models/Results/TableResult.cs
@@ -1,16 +1,29 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Web;
 
 namespace KenticoInspector.Core.Models.Results
 {
     public class TableResult<T> : Result
     {
-        public IEnumerable<T> Rows { get; set; }
+        public IEnumerable<T> Rows { get; }
 
         public override bool HasData => Rows.Any();
 
         internal TableResult(IEnumerable<T> rows)
         {
+            foreach (var row in rows)
+            {
+                foreach (var property in row.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
+                {
+                    if (property.GetIndexParameters().Length == 0 && property.GetValue(row) is string stringValue)
+                    {
+                        property.SetValue(row, HttpUtility.HtmlEncode(stringValue));
+                    }
+                }
+            }
+
             Rows = rows;
         }
 
